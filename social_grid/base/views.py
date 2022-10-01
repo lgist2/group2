bddecf797902ad44ad723fc8aeaf781fca9f559a
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-09-30 22:25:34 -0500

    Updated Base w/ new Create Post items
    This commit updates base with a new media folder for when users post a picture. It also updates the nav bar to allow users to migrate to a Create Post page.

--- social_grid/base/views.py
+++ social_grid/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from post.forms import AddPostForm
+
 
 # Create your views here.
 @login_required
