fdb9cd543f8857e5c929ec5c407dc8af138648e6
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-09-30 22:29:25 -0500

    Updated User Profile Page
    With the addition of the user being able to create posts, they now can view on their profile page what posts they have created. With the help of the Post app, their profile is also where they will be able to edit and delete posts.

--- social_grid/users/views.py
+++ social_grid/users/views.py
@@ -1,7 +1,9 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from post.models import AddPost
 
 
 def registration(request):
@@ -10,7 +12,7 @@ def registration(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You now can login!')
+            messages.success(request, f'Your account has been created! You now can login!') #need to add to html instead 
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -18,4 +20,17 @@ def registration(request):
 
 @login_required()
 def profile(request):
-    return render(request, 'users/profile.html')
+    not_exists = False
+    active_account = AddPost()
+    active_account.account = request.user
+    current_posts = AddPost.objects.all()
+    user_posts = AddPost.objects.filter(account=active_account.account)
+    if current_posts.exists():
+        for posts in current_posts:
+            if user_posts == posts.account:
+                return render(request, 'users/profile.html', {'user_posts' : user_posts, 'not_exists': not_exists})
+            else:
+                return render(request, 'users/profile.html', {'user_posts' : user_posts, 'not_exists': not_exists})
+    else:
+        not_exists = True
+        return render(request, 'users/profile.html',{'user_posts' : user_posts, 'not_exists': not_exists})
