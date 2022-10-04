92adf33734e3425a6adcf1aeba4d910f625efb4e
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-10-03 23:26:06 -0500

    US01 - User can now update their profile

--- social_grid/users/models.py
+++ social_grid/users/models.py
@@ -1,3 +1,13 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from PIL import Image
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(default=now)
+    profile_img = models.ImageField(default='profile_images/logo-only-nobg.png', upload_to='profile_images/')
+
+    def __str__(self):
+        return self.user
