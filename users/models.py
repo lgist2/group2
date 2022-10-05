bb68f8a270794eb81b658f3676d5ae9ba3faf0be
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-10-05 01:03:25 -0500

    US01 and issue fixes
    User can now update their profile without bugs. User can also have a profile picture and edit it as well.

---
+++ users/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from email.mime import image
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from ckeditor.fields import RichTextField
+from PIL import Image
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(default=now)
+    bio = RichTextField(blank=True, null=True)
+    image = models.ImageField(default='profile_images/logo-only-nobg.png', upload_to='profile_images/')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
