3caa39c6d52389107e1a9a5f9fd5a0f5c6bce680
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-10-05 20:10:29 -0500

    US01
    User has a rough view of posts on home page. Working on changing some functions for more efficient code.

--- social_grid/base/urls.py
+++ social_grid/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views 
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
+    path('', PostListView.as_view(), name='home-page'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     
 ]
--- social_grid/base/views.py
+++ social_grid/base/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
 from post.forms import AddPostForm
+from django.views.generic import ListView, DetailView
+
+from post.models import AddPost
 
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'base/home.html')
+    context = {
+        'posts' : AddPost.objects.all()
+    }
+    return render(request, 'base/home.html', context)
+
+
+class PostListView(ListView):
+    model = AddPost
+    template_name = 'base/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = AddPost
