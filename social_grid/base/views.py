6a919dbfd3c6be0db7b0d823f5340c9a2c89dbd6
Levi Gist <ligist2@gmail.com>
2022-10-27 22:52:57 -0500

    Able to view likes and comments

--- social_grid/base/views.py
+++ social_grid/base/views.py
@@ -13,10 +13,15 @@ from post.models import Post
 def home(request):
     #gets active user
     #filters the posts and displays only the posts of the users friends
+    liked = False
     active_user = request.user
     posts = Post.objects.filter(account__friends=active_user.account).order_by("-id")
+    likes = Post.objects.filter(likes=active_user)
+    if likes.exists():
+        liked = True
     context = {
         'posts' : posts,
+        'liked' : liked,
     }
     return render(request, 'base/home.html', context)
 
