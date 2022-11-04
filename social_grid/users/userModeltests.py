ee88528f39748304d4aef5222fe18d3faf81b533
rlb32 <93962719+rlb32@users.noreply.github.com>
2022-11-04 09:49:48 -0500

    Unit testing for two of the features

---
+++ social_grid/users/userModeltests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+from users.models import Account, FriendRequest
+
+class AccountModelTestCases(TestCase):
+    
+
+
+
+
+class FriendRequestModelTestCases(TestCase):
+    
