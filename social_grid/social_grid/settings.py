d1e1a773bb262a8873043c41be0a78dedac79dd1
Levi Gist <71097489+lgist2@users.noreply.github.com>
2022-11-29 20:28:29 -0600

    spacing

--- social_grid/social_grid/settings.py
+++ social_grid/social_grid/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = 'django-insecure-^t6b=u)=d5ufd0am5sc1x_f6-gte!h^8v8@ogu5rvc5cy==4$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 # Application definition
 
 INSTALLED_APPS = [
